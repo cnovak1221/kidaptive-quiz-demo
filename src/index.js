e6e53d8aa8edd8bc212465565a1a9d889ecbf9af
Solomon Liu <sliu198@gmail.com>
2017-03-28 00:45:26 -0700

    index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import CreateLearner from './js/CreateLearner';
+import QuizPrompt from './js/QuizPrompt';
 
 let createLearner = ReactDOM.render(
-    <CreateLearner/>,
+    // <CreateLearner/>,
+    <QuizPrompt/>,
     document.getElementById('root')
 );
 
 window.KidaptiveSdk.init('gPt1fU+pTaNgFv61Qbp3GUiaHsGcu+0h8', {version:"1.0.0", build:"1"}).then(function(sdk) {
     window.sdk = sdk;
-    //bypass prompt category validation
+
+    //bypass promptCategory validation; Category data missing.
     sdk.modelManager.promptCategories = {};
-    createLearner.setState({sdkReady: true});
+
+    // createLearner.setState({sdkReady: true});
+    window.learner = sdk.getLearnerList()[0];
+    sdk.startTrial(window.learner.id);
 
     return sdk;
 });
